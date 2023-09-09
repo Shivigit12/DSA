06ea50adc08af9c3a1ca89f99632238dc9609e05
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-09-09 16:29:34 +0530

    Time: 48 ms (73.92%), Space: 128 MB (21.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -4,59 +4,48 @@ class ListNode {
     int key;
     int val;
     
-    // Constructor for ListNode
     public ListNode(int key, int val) {
         this.next = null;
         this.prev = null;
-        this.val = val;
         this.key = key;
+        this.val = val;
     }
 }
-
 class LRUCache {
-    // HashMap to store key-value pairs
+    //HashMap to store key value pairs
     HashMap<Integer, ListNode> map = new HashMap<>();
-    // Sentinel nodes for Doubly Linked List
+    //Sentinel nodes for doubly linked list
     ListNode head = new ListNode(0, 0);
     ListNode tail = new ListNode(0, 0);
-    int size; // Maximum capacity of the cache
+    int size;//Max capacity of the cache
     
-    // Constructor to initialize the LRUCache
     public LRUCache(int capacity) {
         this.size = capacity;
-        // Connect the head and tail sentinels to create an empty Doubly Linked List
+        //connect the head and tail sentinels to create an empty doubly linked list
         head.next = tail;
         tail.prev = head;
     }
     
-    // Get the value associated with a key
     public int get(int key) {
-        if (map.containsKey(key)) {
+        if(map.containsKey(key)) {
             ListNode temp = map.get(key);
-            // Move the accessed node to the front of the list (indicating it's the most recently used)
+            //Move the accessed node to the front of the list
             deleteNode(temp);
             addNode(temp);
             return temp.val;
-        } else {
-            return -1; // Cache miss
+        }else{
+            return -1; //Cache miss
         }
     }
     
-    // Put a key-value pair in the cache
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            // Key already exists, so delete the existing node
+        if(map.containsKey(key))
             deleteNode(map.get(key));
-        }
-        if (map.size() == size) {
-            // If the cache is full, remove the least recently used item (tail.prev)
+        if(map.size() == size)
             deleteNode(tail.prev);
-        }
-        // Create a new node and add it to the front of the list and the HashMap
         addNode(new ListNode(key, value));
     }
-
-    // Helper function to add a node to the front of the Doubly Linked List
+    
     public void addNode(ListNode node) {
         map.put(node.key, node);
         node.next = head.next;
@@ -64,15 +53,12 @@ class LRUCache {
         head.next = node;
         node.prev = head;
     }
-
-    // Helper function to delete a node from the Doubly Linked List
     public void deleteNode(ListNode node) {
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 }
-
 
 /**
  * Your LRUCache object will be instantiated and called as such:
