3a0e3708f956c05e6874c887db19e8208d54d6d5
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-10-15 12:00:49 +0530

    Time: 2 ms (98.02%), Space: 40.4 MB (69.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < arr.length; i++) {
             if(map.containsKey(arr[i]))
                 map.put(arr[i], map.get(arr[i]) + 1);
             else
                 map.put(arr[i], 1);
         }
-        HashSet<Integer> set = new HashSet<>();
         for(Map.Entry<Integer, Integer> m : map.entrySet()) {
             int k = m.getValue();
             if(set.contains(k))
