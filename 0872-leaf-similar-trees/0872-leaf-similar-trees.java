2dd474aaa8b9d82571e94ae3b35ac7d4d9c702dd
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-10-29 16:05:53 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (18.41%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -17,19 +17,17 @@ class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
-        dfs(root1, list1);
-        dfs(root2, list2);
+        dfs(list1, root1);
+        dfs(list2, root2);
         return list1.equals(list2);
     }
     
-    public void dfs(TreeNode root, List<Integer> list) {
+    public void dfs(List<Integer> list, TreeNode root) {
         if(root == null)
             return;
-        if(root.left == null && root.right == null) {
+        if(root.left == null && root.right == null)
             list.add(root.val);
-        }
-        
-        dfs(root.left, list);
-        dfs(root.right, list);
+            dfs(list, root.left);
+            dfs(list, root.right);
     }
 }
