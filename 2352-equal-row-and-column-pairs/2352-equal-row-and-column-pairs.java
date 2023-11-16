809c5dfd69947817dddde854457bf38082323e03
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-11-16 21:53:49 +0530

    Time: 57 ms (28.89%), Space: 54.3 MB (5.21%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,31 +1,28 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int n = grid.length;
         HashMap<String, Integer> map1 = new HashMap<>();
         HashMap<String, Integer> map2 = new HashMap<>();
+        int n = grid.length;
         for(int i = 0; i < n; i++) {
-            StringBuilder s = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for(int j = 0; j < n; j++) {
-                s.append(grid[i][j]);
-                s.append(":");
+                sb.append(grid[i][j] + ":");
             }
-            String str = s.toString();
+            String str = sb.toString();
             map1.put(str, map1.getOrDefault(str, 0) + 1);
         }
         for(int i = 0; i < n; i++) {
-            StringBuilder s = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for(int j = 0; j < n; j++) {
-                s.append(grid[j][i]);
-                s.append(":");
+                sb.append(grid[j][i] + ":");
             }
-            String str = s.toString();
+            String str = sb.toString();
             map2.put(str, map2.getOrDefault(str, 0) + 1);
         }
         int ans = 0;
         for(String s : map1.keySet()) {
-            if(map2.containsKey(s)) {
-                ans += map1.get(s) * map2.get(s);
-            }
+            if(map2.containsKey(s))
+                ans = ans + map1.get(s) * map2.get(s);
         }
         return ans;
     }
