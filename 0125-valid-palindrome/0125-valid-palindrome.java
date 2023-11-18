a6b06e3ee188e43290af91a9b669bddbcb439bc9
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-11-18 13:28:42 +0530

    Time: 22 ms (24.54%), Space: 51.2 MB (5.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        if(Palindrome(s, 0))
-            return true;
-        return false;
-        
-        
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        return isValid(str, 0);
     }
-    public boolean Palindrome(String s, int i){
     
-    if(i>=s.length()/2) return true;
-            
-            // If start is not equal to end, not palindrome.
-            if(s.charAt(i)!=s.charAt(s.length()-i-1)) return false;
-            
-            // If both characters are same, increment i and check start+1 and end-1.
-            return Palindrome(s, i+1);
+    public boolean isValid(String str, int i) {
+        if(i >= str.length() / 2)
+            return true;
+        if(str.charAt(i) != str.charAt(str.length() - 1 - i))
+            return false;
+        return isValid(str, i + 1);
     }
 }
