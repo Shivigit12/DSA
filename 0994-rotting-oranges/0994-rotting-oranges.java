4430623a5e2d7fe4241b0ef50cca72413602a23b
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2023-11-30 18:45:56 +0530

    Time: 2 ms (84.12%), Space: 41.1 MB (61.23%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,62 +1,38 @@
-class Pair{
-    int r;
-    int c;
-    int t;
-    Pair(int r, int c, int t){
-        this.r = r;
-        this.c = c;
-        this.t = t;
-    }
-}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int minutes = -1;
+        int freshCount = 0;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        int ans_time = 0;
-        Queue<Pair> q = new LinkedList<>();
-        int[][] visited = new int[n][m]; // to mark roten oranges
-        int countFresh = 0; // Counting Freash orange
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2) q.offer(new int[]{i, j}); //gathering rotten oranges to queue
+                else if(grid[i][j] == 1) freshCount++;
+            }
+        }
 
-        // travers in grid to find out possible position of rotten orange and also count freash oranges
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
-                    q.add(new Pair(i,j,0));
-                    visited[i][j] = 2;
-                }else if(grid[i][j] == 1){
-                    countFresh++;
-                }else{
-                    visited[i][j] = 0;
+        if(freshCount == 0) return 0; //there is no fresh orange.
+        if(q.size() == 0) return -1; //there is noting to rotten.
+        while(!q.isEmpty()) {
+            minutes++;
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                int[] node = q.poll();
+                for(int j = 0; j < dirs.length; j++) {
+                    int x = node[0] + dirs[j][0];
+                    int y = node[1] + dirs[j][1];
+                    if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        freshCount--;
+                        q.offer(new int[]{x, y});
+                    }
                 }
             }
+            
         }
-
-        int[] delRow = {-1, 0, +1, 0};
-        int[] delCol = {0, +1, 0, -1};
-        int cnt = 0; // keep track of rotten orange
-        // BFS Traversal
-        while(!q.isEmpty()){
-            int row = q.peek().r;
-            int col = q.peek().c;
-            int time = q.peek().t;
-            ans_time = Math.max(ans_time, time);
-            q.remove();
-            // now i have to check for neighbours in four directions
-            for(int i = 0; i < 4; i++){
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == 0 && 
-                grid[nrow][ncol] == 1){
-                    q.add(new Pair(nrow, ncol, time + 1));
-                    visited[nrow][ncol] = 2;
-                    cnt++;
-                }   
-            }
-        }
-        
-        if(cnt != countFresh) return -1;
-        return ans_time;
+        return freshCount != 0 ? -1 : minutes;
     }
 }
