413d5361542dfd389cc80970b5df9856cf334ab0
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-01-05 00:40:54 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (8.70%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int minDiff = Integer.MAX_VALUE;
+    int prev = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        
+        getMinimum(root);
+        return minDiff;
+    }
+    
+    public void getMinimum(TreeNode root) {
+        if(root.left != null)
+            getMinimum(root.left);
+        minDiff = Math.min(minDiff, Math.abs(root.val - prev));
+        prev = root.val;
+        if(root.right != null)
+            getMinimum(root.right);
+    }
+}
