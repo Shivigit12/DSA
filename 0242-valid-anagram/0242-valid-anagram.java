7b9de2d1fce48c646c9ce39055b92af0c67a2300
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-03-10 14:24:08 +0530

    Time: 17 ms (20.83%), Space: 43.9 MB (60.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            if(map1.containsKey(s.charAt(i)))
-                map1.put(s.charAt(i), map1.get(s.charAt(i)) + 1);
-            else
-                map1.put(s.charAt(i), 1);
+        if(s.length() != t.length())
+            return false;
+        HashMap<Character, Integer> map = new HashMap<>(); 
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        System.out.println(map1);
-        for(int i = 0; i < t.length(); i++){
-            if(map2.containsKey(t.charAt(i)))
-                map2.put(t.charAt(i), map2.get(t.charAt(i)) + 1);
-            else
-                map2.put(t.charAt(i), 1);
+        for(int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if(map.containsKey(ch) && map.get(ch) > 0) {
+                map.put(ch, map.get(ch) - 1);
+            } else {
+                return false;
+            }
         }
-                System.out.println(map2);
-
-        if(map1.equals(map2))
-            return true;
-        return false;
+        return true;
     }
 }
