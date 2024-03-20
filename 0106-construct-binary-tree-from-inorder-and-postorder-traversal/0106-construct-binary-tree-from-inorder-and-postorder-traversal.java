e6b3c9e1e5b5b44013e89da573d2c28fab070330
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-03-20 22:49:26 +0530

    Time: 3 ms (33.74%), Space: 44.1 MB (75.25%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,25 +15,26 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder == null || postorder == null || inorder.length != postorder.length)
-            return null;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
     
-    private TreeNode buildTree(int[] inorder, int istart, int iend, int[] postorder, int pstart, int pend, HashMap<Integer,Integer> map) {
-        if(istart > iend || pstart > pend)
-            return null;
-        TreeNode root = new TreeNode(postorder[pend]);
-        int inRoot = map.get(postorder[pend]);
-        int numsLeft = inRoot - istart;
-        root.left = buildTree(inorder, istart, inRoot - 1, postorder, pstart, pstart + numsLeft - 1, map);
-        root.right = buildTree(inorder, inRoot + 1, iend, postorder, pstart + numsLeft, pend - 1, map);
+    public TreeNode build(int[] inorder, int inS, int inE, int[] postorder, int posS, int posE){
+        if(inS > inE || posS > posE)
+            return  null;
+        
+        TreeNode root = new TreeNode(postorder[posE]);
+        
+        int rootI = 0;
+        for(int i = 0; i < inorder.length; i++){
+            if(inorder[i] == root.val){
+                rootI = i;
+                break;
+            }
+        }
+        
+        root.left = build(inorder, inS, rootI - 1, postorder, posS, posS + rootI - inS - 1);
+        root.right = build(inorder, rootI + 1, inE, postorder, posS + rootI - inS, posE - 1);
+        
         return root;
-    } 
-    
-    
+    }
 }
