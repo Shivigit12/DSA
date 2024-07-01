dfa488625af20878ffa10d0ae49be5e9908b57d1
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-02 00:05:32 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (52.59%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,40 +1,9 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('a', 0);
-        map.put('b', 0);
-        map.put('c', 0);
-        map.put('d', 0);
-        map.put('e', 0);
-        map.put('f', 0);
-        map.put('g', 0);
-        map.put('h', 0);
-        map.put('i', 0);
-        map.put('j', 0);
-        map.put('k', 0);
-        map.put('l', 0);
-        map.put('m', 0);
-        map.put('n', 0);
-        map.put('o', 0);
-        map.put('p', 0);
-        map.put('q', 0);
-        map.put('r', 0);
-        map.put('s', 0);
-        map.put('t', 0);
-        map.put('u', 0);
-        map.put('v', 0);
-        map.put('w', 0);
-        map.put('x', 0);
-        map.put('y', 0);
-        map.put('z', 0);
-        for(int i = 0; i < sentence.length(); i++) {
-            if(map.containsKey(sentence.charAt(i)))
-                map.put(sentence.charAt(i), map.get(sentence.charAt(i)) + 1);
-            else
-                return false;
-        }
-        for(Map.Entry<Character, Integer> m : map.entrySet()) {
-            if(m.getValue() < 1)
+        if(sentence.length() < 26)
+            return false;
+        for(int i = 1; i <= 26; i++) {
+            if(sentence.indexOf((char)i + 96) < 0)
                 return false;
         }
         return true;
