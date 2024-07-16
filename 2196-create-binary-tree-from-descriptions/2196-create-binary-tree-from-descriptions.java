cdcdfb46dd7969c92a7727e8b0d385128a3ef841
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-16 13:46:06 +0530

    Time: 81 ms (22.93%), Space: 55.1 MB (87.26%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -15,27 +15,23 @@
  */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        Map<Integer, TreeNode> mp = new HashMap<>();
-        Set<Integer> hasParent = new HashSet<>();
-        
-        for (int i = 0; i < descriptions.length; i++) {
-            if (!mp.containsKey(descriptions[i][0]))
-                mp.put(descriptions[i][0], new TreeNode(descriptions[i][0]));
-            if (!mp.containsKey(descriptions[i][1]))
-                mp.put(descriptions[i][1], new TreeNode(descriptions[i][1]));
-            hasParent.add(descriptions[i][1]);
+        HashMap<Integer, TreeNode> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < descriptions.length; i++) {
+            if(!map.containsKey(descriptions[i][0])) 
+                map.put(descriptions[i][0], new TreeNode(descriptions[i][0]));
+            if(!map.containsKey(descriptions[i][1]))
+                map.put(descriptions[i][1],new TreeNode(descriptions[i][1]));
+            set.add(descriptions[i][1]);
         }
-        
         TreeNode root = null;
-        for (int i = 0; i < descriptions.length; i++) {
-            if (descriptions[i][2] == 1) { // left
-                mp.get(descriptions[i][0]).left = mp.get(descriptions[i][1]);
-            } else { // right
-                mp.get(descriptions[i][0]).right = mp.get(descriptions[i][1]);
-            }
-            if (!hasParent.contains(descriptions[i][0])) {
-                root = mp.get(descriptions[i][0]);
-            }
+        for(int i = 0; i < descriptions.length; i++) {
+            if(descriptions[i][2] == 1)
+                map.get(descriptions[i][0]).left = map.get(descriptions[i][1]);
+            else
+                map.get(descriptions[i][0]).right = map.get(descriptions[i][1]);
+            if(!set.contains(descriptions[i][0]))
+                root = map.get(descriptions[i][0]);
         }
         return root;
     }
