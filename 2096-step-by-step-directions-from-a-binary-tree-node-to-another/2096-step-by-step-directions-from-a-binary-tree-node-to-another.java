9c08bb11061f9ad8c0042d4e80578adf84506f64
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-21 18:52:30 +0530

    Time: 18 ms (91.65%), Space: 80.1 MB (45.99%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -14,25 +14,28 @@
  * }
  */
 class Solution {
-    
-        private boolean find(TreeNode n, int val, StringBuilder sb) {
-        if (n.val == val)
-            return true;
-        if (n.left != null && find(n.left, val, sb))
-            sb.append("L");
-        else if (n.right != null && find(n.right, val, sb))
-            sb.append("R");
-        return sb.length() > 0;
-    }
-
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        StringBuilder s = new StringBuilder(), d = new StringBuilder();
+        if(root == null)
+            return "";
+        StringBuilder s = new StringBuilder();
+        StringBuilder d = new StringBuilder();
         find(root, startValue, s);
         find(root, destValue, d);
-        int i = 0, max_i = Math.min(d.length(), s.length());
-        while (i < max_i && s.charAt(s.length() - i - 1) == d.charAt(d.length() - i - 1))
-            ++i;
+        int i = 0;
+        int max_i = Math.min(s.length(), d.length());
+        while(i < max_i && s.charAt(s.length() - i - 1) == d.charAt(d.length() - i - 1))
+            i++;
         return "U".repeat(s.length() - i) + d.reverse().toString().substring(i);
+        
+    }
     
+    public boolean find(TreeNode root, int value, StringBuilder sb) {
+        if(root.val == value)
+            return true;
+        if(root.left != null && find(root.left, value, sb))
+            sb.append("L");
+        else if(root.right != null && find(root.right, value, sb))
+            sb.append("R");
+        return sb.length() > 0;
     }
 }
