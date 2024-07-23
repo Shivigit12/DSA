9e29de6269cceea3aa18d16f6531f83f83b8649d
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-23 23:46:28 +0530

    Time: 5 ms (92.24%), Space: 44.3 MB (59.67%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -4,22 +4,19 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            
         }
-        System.out.print(map + " ");
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            minHeap.add(new int[] {entry.getKey(), entry.getValue()});
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
         int index = 0;
-        while(!minHeap.isEmpty()) {
-            int[] min = minHeap.poll(); 
+        for(Map.Entry<Integer, Integer> m : map.entrySet()) {
+            pq.add(new int[] {m.getKey(), m.getValue()});
+        }
+        while(!pq.isEmpty()) {
+            int min[] = pq.poll();
             while(index < nums.length && min[1] > 0) {
-                nums[index++] = min[0];
+                ans[index++] = min[0];
                 min[1]--;
             }
         }
-        
-        return nums;
+        return ans;
     }
 }
