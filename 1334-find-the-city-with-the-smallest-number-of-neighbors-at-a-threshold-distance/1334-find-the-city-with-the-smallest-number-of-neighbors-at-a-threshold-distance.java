7e1f616ee486ac3cdeccbeb471ba55773138f8b9
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-28 21:09:16 +0530

    Time: 7 ms (97.38%), Space: 44.2 MB (67.47%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,63 +1,41 @@
 class Solution {
      public int findTheCity(int n, int[][] edges, int thresh) {
-        // floyd warshall - O(n^3)
-        int max = Integer.MAX_VALUE;
-        int [][] graph = new int[n][n];
-        
-// initialize the graph with maximum values
-        for(int i = 0; i < n; i ++)
-            for(int j = 0; j < n; j ++ )
-                if( i != j )
-                    graph[i][j] = max;
-
-//initialize it with the edge weights provided
-        for(int e[] : edges){
-            graph[e[0]][e[1]] = e[2];
-            graph[e[1]][e[0]] = e[2];
-        }
-
-// printing that graph
-       for(int i = 0; i < n; i ++){
-            for(int j = 0; j < n; j ++ ){
-                if (graph[i][j] == max) 
-                    System.out.print("INF\t");
+        int matrix[][] = new int[n][n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(j==i)
+                matrix[i][j] = 0;
                 else
-                    System.out.print(graph[i][j] +"\t");
-            }
-            System.out.println();
-        }            
-        System.out.println();
-
-        for(int i = 0; i < n; i ++ )
-            for(int j = 0; j < n; j ++)
-                for(int k = 0; k < n; k ++ )
-                    if(graph[j][i] != max && graph[i][k] != max)
-                        graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
-
-//printing that graph
-        for(int i = 0; i < n; i ++){
-            for(int j = 0; j < n; j ++ ){
-                if (graph[i][j] == max) 
-                    System.out.print("INF\t");
-                else
-                    System.out.print(graph[i][j] +"\t");
-            }
-            System.out.println();
-        }
-
-//finding the city with minimum number of cities with paths at most = threshold value
-        int node = 0;
-        for(int i = 0; i < n; i ++){
-            int c = 0; // counting the number of cities
-            for(int j = 0; j < n; j ++ ){
-                if(graph[i][j] <= thresh)
-                    c ++;
-            }
-            if( c <= max ){
-                node = i;
-                max = c;
+                matrix[i][j] = (int)(1e9);
             }
         }
-        return node;
+        for(int i=0;i<edges.length;i++) {
+            matrix[edges[i][0]][edges[i][1]] = edges[i][2];
+            matrix[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+        for(int k=0;k<n;k++) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    matrix[i][j] = Math.min(matrix[i][j],(matrix[i][k] + matrix[k][j]));
+                }
+            }
+        }
+        int minCount = Integer.MAX_VALUE;
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++) {
+            int count = 0;
+            for(int j=0;j<n;j++) {
+                if(i==j)
+                continue;
+                if(matrix[i][j] <= thresh)
+                count++;
+            }
+            if(count <= minCount) {
+                minCount = count;
+                if(i > ans)
+                ans = i;
+            }
+        }
+        return ans;
     }
 }
