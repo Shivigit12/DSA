e85549c854f991d1de6f74f3a064b181f73b07a0
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-28 10:13:27 +0530

    Time: 51 ms (100.00%), Space: 40.9 MB (100.00%) - LeetHub

--- 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
+++ 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
@@ -1,30 +1,30 @@
 class Solution {
     public int nonSpecialCount(int l, int r) {
-        int st=(int)Math.sqrt(l);
-        int end = (int)Math.sqrt(r);
-        if(!isPerfectSquare(l)){
+        int st = (int) Math.sqrt(l);
+        int end = (int) Math.sqrt(r);
+        if(!isPerfectSquare(l))
             st++;
+        int totalP = 0;
+        for(int i = st; i <= end; i++) {
+            if(isPrime(i))
+                totalP++;
         }
-        int totalPrimes = 0;
-        for(int i = st;i<=end;i++){
-            if(isPrime(i)){
-                totalPrimes++;
-            }
-        }
-        int ans = r-l+1-totalPrimes;
-        return ans;
+        return r - l + 1 - totalP;
     }
+    
     public boolean isPerfectSquare(int n) {
-        if (n < 0) {
-            return false; // Negative numbers cannot be perfect squares
-        }
-        int sqrt = (int) Math.sqrt(n);
-        return (sqrt * sqrt == n);
+        if(n < 0)
+            return false;
+        int num = (int) Math.sqrt(n);
+        return num * num == n;
     }
-    static boolean isPrime(int x){
-        if(x == 1) return false;
-        for(int i = 2 ; i * i <= x ; i++){
-            if(x % i == 0) return false;
+    
+    public boolean isPrime(int num) {
+        if(num == 1)
+            return false;
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0)
+                return false;
         }
         return true;
     }
