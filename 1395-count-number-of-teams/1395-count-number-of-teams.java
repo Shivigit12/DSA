a72698798b3faa005737ad6e398e6f35bf1e6734
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-07-29 20:46:34 +0530

    Time: 16 ms (82.28%), Space: 42.1 MB (86.92%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,14 +1,25 @@
 class Solution {
     public int numTeams(int[] rating) {
         int count = 0;
-        for(int i = 0; i < rating.length; i++) {
-            for(int j = i + 1; j < rating.length; j++) {
-                for(int k = j + 1; k < rating.length; k++) {
-                    if((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
-                }
+        for(int j = 0; j < rating.length; j++) {
+            int smaller_left = 0;
+            int bigger_left = 0;
+            int smaller_right = 0;
+            int bigger_right = 0;
+            for(int i = j - 1; i >= 0; i--) {
+                if(rating[i] < rating[j])
+                    smaller_left++;
+                else if(rating[i] > rating[j])
+                    bigger_left++;
             }
+            for(int k = j + 1; k < rating.length; k++) {
+                if(rating[k] > rating[j])
+                    bigger_right++;
+                else if(rating[k] < rating[j])
+                    smaller_right++;
+            }
+            count += smaller_left * bigger_right + bigger_left * smaller_right;
+            
         }
         return count;
     }
