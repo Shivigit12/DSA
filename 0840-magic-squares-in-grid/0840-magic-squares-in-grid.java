26fc80e97d016ad8f33d5564afdb27e40ce29f2a
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-08-09 23:40:07 +0530

    Time: 1 ms (41.08%), Space: 41.6 MB (26.49%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,89 +1,41 @@
 class Solution {
-    public int findrowsum(int grid[][], int r, int c) {
-        boolean visited[] = new boolean[10];
-        int sum = 0;
-        for (int i = 0; i < 3; i++) {
-            int rowsum = 0;
-            for (int j = 0; j < 3; j++) {
-                int val = grid[r + i][c + j];
-                if (val == 0 || val >= 10 || visited[val]) {
-                    return -1;
-                }
-                visited[val] = true;
-                rowsum += val;
-            }
-            if (i == 0) {
-                sum = rowsum;
-            } else {
-                if (sum != rowsum) {
-                    return -1;
+    public int numMagicSquaresInside(int[][] grid) {
+        int count = 0;
+        if(grid.length < 3)
+            return 0;
+        else {
+            for(int i = 0; i < grid.length - 2; i++) {
+                for(int j = 0; j < grid[0].length - 2; j++) {
+                    if(magicNumbers(grid, i , j))
+                        count++;
                 }
             }
         }
-        return sum;
+        return count;
     }
-
-    public int findcolsum(int grid[][], int r, int c) {
-        int sum = 0;
-        for (int j = 0; j < 3; j++) {
-            int colsum = 0;
-            for (int i = 0; i < 3; i++) {
-                int val = grid[r + i][c + j];
-                colsum += val;
-            }
-            if (j == 0) {
-                sum = colsum;
-            } else {
-                if (sum != colsum) {
-                    return -1;
-                }
+    
+    public boolean magicNumbers(int[][] grid, int r, int c) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = r; i < r + 3; i++) {
+            for(int j = c; j < c + 3; j++) {
+                if(!set.isEmpty() && set.contains(grid[i][j]))
+                    return false;
+                if(grid[i][j] > 9 || grid[i][j] < 1)
+                    return false;
+                set.add(grid[i][j]);
             }
         }
-        return sum;
-    }
-
-    public int finddiagonalsum(int grid[][], int r, int c) {
-        int sum1 = grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2];
-        int sum2 = grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c];
-        if (sum1 == sum2) {
-            return sum1;
-        }
-        return -1;
-    }
-
-    public boolean magicgrid(int grid[][], int r, int c) {
-        int rowsum = findrowsum(grid, r, c);
-        if (rowsum == -1)
-            return false;
-        int colsum = findcolsum(grid, r, c);
-        if (colsum == -1)
-            return false;
-        int diagonalsum = finddiagonalsum(grid, r, c);
-        if (diagonalsum == -1)
-            return false;
-        if (rowsum == colsum && rowsum == diagonalsum) {
+        int r1 = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
+        int r2 = grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2];
+        int r3 = grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2];
+        int c1 = grid[r][c] + grid[r + 1][c] + grid[r + 2][c];
+        int c2 = grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1];
+        int c3 = grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2];
+        int d1 = grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2];
+        int d2 = grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c];
+        if(r1 == r2 && r2 == r3 && r3 == c1 && c1 == c2 && c2 == c3 && c3 == d1 && d1 == d2){
             return true;
         }
         return false;
-    }
-
-    public int numMagicSquaresInside(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int count = 0;
-        if (row < 3 || col < 3) {
-            count = 0;
-        } else {
-            for (int i = 0; i < row - 2; i++) {
-                for (int j = 0; j < col - 2; j++) {
-                    if (magicgrid(grid, i, j)) {
-                        count++;
-                    }
-                }
-            }
-
-        }
-
-        return count;
     }
 }
