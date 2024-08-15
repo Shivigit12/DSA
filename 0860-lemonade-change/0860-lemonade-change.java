ea2cb93945ed5136917f349c7aadcb3de541481f
Shivigit12 <111731350+Shivigit12@users.noreply.github.com>
2024-08-15 09:23:43 +0530

    Time: 2 ms (95.27%), Space: 55.9 MB (48.09%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int fiveC = 0;
+        int tenC = 0;
+      
+        for(int i = 0; i < bills.length; i++) {
+            if(bills[i] == 5) {
+                fiveC++;
+            } else if(bills[i] == 10) {
+                if(fiveC > 0) {
+                    fiveC--;
+                tenC++;
+                } else
+                    return false;
+                
+            } else if(bills[i] == 20) {
+                if (fiveC > 0 && tenC > 0) {
+                    fiveC--;
+                    tenC--;
+                } else if (fiveC > 2) {
+                    fiveC -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+       
+        return true;
+    }
+}
